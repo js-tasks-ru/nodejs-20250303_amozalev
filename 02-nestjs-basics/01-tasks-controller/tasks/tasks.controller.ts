86088824287dc18cb914adc410ffd975d3f5c643
amozalev <mozalev.alexey@gmail.com>
2025-03-15 16:04:54 +0200

    Implemented tasks controller and service.

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.controller.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.controller.ts
@@ -3,6 +3,8 @@ import {
   Controller,
   Delete,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
   Patch,
   Post,
@@ -15,17 +17,42 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {}
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
 
   @Get(":id")
-  getTaskById(@Param("id") id: string) {}
+  getTaskById(@Param("id") id: string) {
+    const task = this.tasksService.getTaskById(id);
+    if (task) {
+      return task;
+    }
+
+    throw new HttpException("Wrong task id", HttpStatus.NOT_FOUND);
+  }
 
   @Post()
-  createTask(@Body() task: Task) {}
+  createTask(@Body() task: Task) {
+    return this.tasksService.createTask(task);
+  }
 
   @Patch(":id")
-  updateTask(@Param("id") id: string, @Body() task: Task) {}
+  updateTask(@Param("id") id: string, @Body() task: Task) {
+    const updatedTask = this.tasksService.updateTask(id, task);
+    if (updatedTask) {
+      return updatedTask;
+    }
+
+    throw new HttpException("Wrong task id", HttpStatus.NOT_FOUND);
+  }
 
   @Delete(":id")
-  deleteTask(@Param("id") id: string) {}
+  deleteTask(@Param("id") id: string) {
+    const removedTask = this.tasksService.deleteTask(id);
+    if (removedTask) {
+      return removedTask;
+    }
+
+    throw new HttpException("Wrong task id", HttpStatus.NOT_FOUND);
+  }
 }
