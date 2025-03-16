758f561ce64b9f95507786a522839533aae74c6c
amozalev <mozalev.alexey@gmail.com>
2025-03-17 00:58:52 +0200

    Fixed tasks.controller.

--- 02-nestjs-basics/02-filtering/tasks/tasks.controller.ts
+++ 02-nestjs-basics/02-filtering/tasks/tasks.controller.ts
@@ -4,15 +4,17 @@ import { SortBy, SortByEnum, TaskStatus } from "./task.model";
 
 @Controller("tasks")
 export class TasksController {
-  constructor(private readonly tasksService: TasksService) {}
+  constructor(private readonly tasksService: TasksService) {
+  }
 
   @Get()
   getTasks(
     @Query("status") status?: TaskStatus,
     @Query("page") page?: number,
     @Query("limit") limit?: number,
-    @Query("sortBy", new ParseEnumPipe(SortByEnum))
+    @Query("sortBy", new ParseEnumPipe(SortByEnum, {optional: true}))
     sortBy?: SortBy,
   ) {
-    return this.tasksService.getTasks(status, page, limit, sortBy);
+    return this.tasksService.getTasks(status, page, limit, sortBy as any);
   }
+}
