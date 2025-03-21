bcf5977f3071d70c76667b9a711c3bae38844940
amozalev <mozalev.alexey@gmail.com>
2025-03-21 20:00:55 +0200

    Added global configuration.

--- 03-di/01-notification-service/app.module.ts
+++ 03-di/01-notification-service/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { TasksModule } from "./tasks/tasks.module";
 import { NotificationsModule } from "./notifications/notifications.module";
 import { NotificationsService } from "./notifications/notifications.service";
 import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [TasksModule, NotificationsModule, UsersModule],
-  providers: [NotificationsService]
+  imports: [
+    ConfigModule.forRoot({ envFilePath: [".env.development"], isGlobal: true }),
+    TasksModule,
+    NotificationsModule,
+    UsersModule,
+  ],
+  providers: [NotificationsService],
 })
 export class AppModule {}
