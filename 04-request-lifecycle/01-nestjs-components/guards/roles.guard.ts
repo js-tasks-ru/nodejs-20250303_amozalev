0d70bbafdf4e43073f9d43e820f4a754583dc38d
amozalev <mozalev.alexey@gmail.com>
2025-03-26 23:26:15 +0200

    Implemented parse-int.pipe, logging.middleware, roles.guard.

--- 04-request-lifecycle/01-nestjs-components/guards/roles.guard.ts
+++ 04-request-lifecycle/01-nestjs-components/guards/roles.guard.ts
@@ -1,5 +1,17 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from "@nestjs/common";
 
 export class RolesGuard implements CanActivate {
-  canActivate(context: ExecutionContext) {}
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const role: string | undefined = request.headers["x-role"];
+    if (role === "admin") {
+      return true;
+    }
+
+    throw new ForbiddenException();
+  }
 }
--- 04-request-lifecycle/01-nestjs-components/middlewares/logging.middleware.ts
+++ 04-request-lifecycle/01-nestjs-components/middlewares/logging.middleware.ts
@@ -2,5 +2,10 @@ import { NestMiddleware } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
 
 export class LoggingMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {}
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, path } = req;
+    console.log(`${method} ${path}`);
+
+    next();
+  }
 }
