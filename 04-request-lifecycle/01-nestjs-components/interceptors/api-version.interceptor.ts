1330d13dec91bd33bfee8691be5037a580cac3f5
amozalev <mozalev.alexey@gmail.com>
2025-03-27 17:37:06 +0200

    Implemented api-version.interceptor.

--- 04-request-lifecycle/01-nestjs-components/interceptors/api-version.interceptor.ts
+++ 04-request-lifecycle/01-nestjs-components/interceptors/api-version.interceptor.ts
@@ -1,5 +1,22 @@
-import { NestInterceptor, ExecutionContext, CallHandler } from "@nestjs/common";
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from "@nestjs/common";
+import { map } from "rxjs";
 
 export class ApiVersionInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler) {}
+  intercept(context: ExecutionContext, next: CallHandler) {
+    const now = Date.now();
+
+    return next.handle().pipe(
+      map((data) => {
+        Object.assign(data, {
+          apiVersion: '1.0',
+          executionTime: Date.now() - now
+        });
+        return data;
+      }),
+    );
+  }
 }
--- 04-request-lifecycle/01-nestjs-components/main.ts
+++ 04-request-lifecycle/01-nestjs-components/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { LoggingMiddleware } from "./middlewares/logging.middleware";
 import { HttpErrorFilter } from "./filters/http-error.filter";
+import { ApiVersionInterceptor } from "./interceptors/api-version.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(new LoggingMiddleware().use);
-  app.useGlobalFilters(new HttpErrorFilter())
+  app.useGlobalFilters(new HttpErrorFilter());
+  app.useGlobalInterceptors(new ApiVersionInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
