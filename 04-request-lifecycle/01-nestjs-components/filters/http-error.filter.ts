2c27283819c6da1b1c398e9aea8882b385e0691e
amozalev <mozalev.alexey@gmail.com>
2025-03-27 16:38:57 +0200

    Implemented http-error.filter.

--- 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
+++ 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
@@ -1,5 +1,25 @@
-import { ArgumentsHost, ExceptionFilter } from "@nestjs/common";
+import { ArgumentsHost, ExceptionFilter, HttpException } from "@nestjs/common";
+import { Response } from "express";
+import * as fs from "node:fs";
+import * as path from "node:path";
 
 export class HttpErrorFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {}
+  private readonly logFile = path.resolve(__dirname, "error.log");
+
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const resp = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+
+    const dateTime = new Date().toISOString();
+    const logMsg = `[${dateTime}] ${status} - ${exception.message}\n`;
+
+    fs.appendFileSync(this.logFile, logMsg);
+
+    resp.status(status).json({
+      statusCode: 400,
+      message: exception.message,
+      timestamp: dateTime,
+    });
+  }
 }
--- 04-request-lifecycle/01-nestjs-components/main.ts
+++ 04-request-lifecycle/01-nestjs-components/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { LoggingMiddleware } from "./middlewares/logging.middleware";
+import { HttpErrorFilter } from "./filters/http-error.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(new LoggingMiddleware().use);
+  app.useGlobalFilters(new HttpErrorFilter())
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
