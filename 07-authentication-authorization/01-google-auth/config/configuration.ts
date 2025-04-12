36128e4844465a498f37237bb33841dbfd796534
amozalev <mozalev.alexey@gmail.com>
2025-04-12 16:22:38 +0300

    Updated configuration to pass tests.

--- 07-authentication-authorization/01-google-auth/config/configuration.ts
+++ 07-authentication-authorization/01-google-auth/config/configuration.ts
@@ -2,12 +2,12 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs("oauth", () => ({
   jwt: {
-    secret: process.env.JWT_SECRET,
+    secret: process.env.JWT_SECRET ?? "killer-is-jim",
     signOptions: { expiresIn: "20s" },
     refreshTokenExpires: "1h",
   },
   google: {
-    secret: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    secret: process.env.GOOGLE_CLIENT_ID ?? "",
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
   },
 }));
