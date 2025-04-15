fa1ceb1b5a6eb86af604eaa21142ea7075e53ce5
amozalev <mozalev.alexey@gmail.com>
2025-04-15 21:27:59 +0300

    Moved mockedTasks, mockTasksRepository methods initialization outside the tests.

--- 08-testing/01-application-tests/tasks/__tests__/tasks.unit.test.ts
+++ 08-testing/01-application-tests/tasks/__tests__/tasks.unit.test.ts
@@ -6,15 +6,41 @@ import { CreateTaskDto } from "../dto/create-task.dto";
 import { UpdateTaskDto } from "../dto/update-task.dto";
 import { getRepositoryToken } from "@nestjs/typeorm";
 
+const DEFAULT_TASKS = [
+  {
+    title: "First task",
+    description: "First task description",
+  },
+  {
+    title: "Second task",
+    description: "Second task description",
+  },
+].map((dto, ind) => ({
+  ...dto,
+  id: ++ind,
+  isCompleted: false,
+}));
+
 describe("TasksService", () => {
   let service: TasksService;
 
-  let mockTasksRepository = {
-    create: jest.fn(),
-    save: jest.fn(),
-    find: jest.fn(),
-    findOneBy: jest.fn(),
-    remove: jest.fn(),
+  let mockedTasks = DEFAULT_TASKS.map((task) => ({ ...task }));
+
+  const mockTasksRepository = {
+    create: jest.fn((dto: CreateTaskDto) => {
+      let lastId = mockedTasks[mockedTasks.length - 1].id ?? 0;
+      return {
+        ...dto,
+        id: ++lastId,
+        isCompleted: false,
+      } as Task;
+    }),
+    save: jest.fn((task: Task) => task),
+    find: jest.fn(() => mockedTasks),
+    findOneBy: jest.fn(({ id: taskId }: { id: number }) =>
+      mockedTasks.find(({ id }) => id === taskId),
+    ),
+    remove: jest.fn((task: Task) => task),
   };
 
   beforeAll(async () => {
@@ -29,6 +55,7 @@ describe("TasksService", () => {
   });
 
   afterEach(() => {
+    mockedTasks = DEFAULT_TASKS.map((task) => ({ ...task }));
     jest.clearAllMocks();
   });
 
@@ -38,67 +65,25 @@ describe("TasksService", () => {
 
   describe("create", () => {
     it("should create a new task", async () => {
-      const mockedDto: CreateTaskDto = {
-        title: "First task",
-        description: "First task description",
+      const dto: CreateTaskDto = {
+        title: "Third task",
+        description: "Third task description",
       };
-      const mockedTask: Task = {
-        ...mockedDto,
-        id: 1,
+
+      let lastId = DEFAULT_TASKS[DEFAULT_TASKS.length - 1].id ?? 0;
+      const task: Task = {
+        ...dto,
+        id: ++lastId,
         isCompleted: false,
       };
-      mockTasksRepository.create.mockReturnValue(mockedTask);
-      mockTasksRepository.save.mockReturnValue(mockedTask);
 
-      const task = new Task();
-      Object.assign(task, mockedDto);
-
-      const savedTask = await service.create(task);
-      expect(savedTask).toEqual(mockedTask);
-
-      // const taskInDb = await repository.findOneBy({ id: 1 });
-      // expect(savedTask).toEqual(taskInDb);
+      const savedTask = await service.create(dto);
+      expect(savedTask).toEqual(task);
     });
   });
 
   describe("findAll", () => {
     it("should return an array of tasks", async () => {
-      const mockedDtos: CreateTaskDto[] = [
-        {
-          title: "First task",
-          description: "First task description",
-        },
-        {
-          title: "Second task",
-          description: "Second task description",
-        },
-      ];
-      const mockedTasks = mockedDtos.map((dto, ind) => ({
-        ...dto,
-        id: ind,
-        isCompleted: false,
-      }));
-
-      mockTasksRepository.create.mockImplementation((dto: CreateTaskDto) => {
-        return mockedTasks.find(
-          ({ title, description }) =>
-            title === dto.title && description === dto.description,
-        );
-      });
-      mockTasksRepository.save.mockImplementation((dto: CreateTaskDto) => {
-        return mockedTasks.find(
-          ({ title, description }) =>
-            title === dto.title && description === dto.description,
-        );
-      });
-      mockTasksRepository.find.mockReturnValue(mockedTasks);
-
-      for (const dto of mockedDtos) {
-        const _task = new Task();
-        Object.assign(_task, dto);
-        await service.create(_task);
-      }
-
       const tasks = await service.findAll();
 
       expect(Array.isArray(tasks)).toEqual(true);
@@ -107,54 +92,16 @@ describe("TasksService", () => {
   });
 
   describe("findOne", () => {
-    let mockedDtos: CreateTaskDto[] = [];
-    let mockedTasks: Task[] = [];
-
-    beforeEach(async () => {
-      mockedDtos = [
-        {
-          title: "First task",
-          description: "First task description",
-        },
-        {
-          title: "Second task",
-          description: "Second task description",
-        },
-      ];
-
-      for (const dto of mockedDtos) {
-        const _task = new Task();
-        Object.assign(_task, dto);
-        await service.create(_task);
-      }
-
-      mockedTasks = mockedDtos.map((dto, ind) => ({
-        ...dto,
-        id: ++ind,
-        isCompleted: false,
-      }));
-    });
-
     it("should return a task when it exists", async () => {
-      mockTasksRepository.findOneBy.mockImplementation(
-        ({ id: taskId }: { id: number }) =>
-          mockedTasks.find(({ id }) => id === taskId),
-      );
-
       const firstTask = await service.findOne(1);
-      expect(firstTask).toEqual(mockedTasks[0]);
+      expect(firstTask).toEqual(DEFAULT_TASKS[0]);
 
       const secondTask = await service.findOne(2);
-      expect(secondTask).toEqual(mockedTasks[1]);
-      expect(firstTask).not.toEqual(mockedTasks[1]);
+      expect(secondTask).toEqual(DEFAULT_TASKS[1]);
+      expect(firstTask).not.toEqual(DEFAULT_TASKS[1]);
     });
 
     it("should throw NotFoundException when task does not exist", async () => {
-      const spy = jest.spyOn(mockTasksRepository, "findOneBy");
-      spy.mockImplementation(
-        (taskId: number) => mockedTasks.find(({ id }) => id === taskId) ?? null,
-      );
-
       try {
         await service.findOne(3);
       } catch (error) {
@@ -164,39 +111,6 @@ describe("TasksService", () => {
   });
 
   describe("update", () => {
-    let mockedDtos: CreateTaskDto[] = [];
-    let mockedTasks: Task[] = [];
-
-    beforeEach(async () => {
-      mockedDtos = [
-        {
-          title: "First task",
-          description: "First task description",
-        },
-      ];
-
-      for (const dto of mockedDtos) {
-        const _task = new Task();
-        Object.assign(_task, dto);
-        await service.create(_task);
-      }
-
-      mockedTasks = mockedDtos.map((dto, ind) => ({
-        ...dto,
-        id: ++ind,
-        isCompleted: false,
-      }));
-
-      mockTasksRepository.findOneBy.mockImplementation(
-        ({ id: taskId }: { id: number }) =>
-          mockedTasks.find(({ id }) => id === taskId) ?? null,
-      );
-      mockTasksRepository.save.mockImplementation(
-        ({ id: taskId }: { id: number }) =>
-          mockedTasks.find(({ id }) => id === taskId) ?? null,
-      );
-    });
-
     it("should update a task when it exists", async () => {
       const firstTask = await service.findOne(1);
       const updatedTask: Task = {
@@ -225,41 +139,11 @@ describe("TasksService", () => {
   });
 
   describe("remove", () => {
-    let mockedDtos: CreateTaskDto[] = [];
-    let mockedTasks: Task[] = [];
-
-    beforeEach(async () => {
-      mockedDtos = [
-        {
-          title: "First task",
-          description: "First task description",
-        },
-      ];
-
-      for (const dto of mockedDtos) {
-        const _task = new Task();
-        Object.assign(_task, dto);
-        await service.create(_task);
-      }
-
-      mockedTasks = mockedDtos.map((dto, ind) => ({
-        ...dto,
-        id: ++ind,
-        isCompleted: false,
-      }));
-
-      mockTasksRepository.findOneBy.mockImplementation(
-        ({ id: taskId }: { id: number }) =>
-          mockedTasks.find(({ id }) => id === taskId) ?? null,
-      );
-      mockTasksRepository.remove.mockImplementation((task: Task) => task);
-    });
-
     it("should remove a task when it exists", async () => {
       const taskId = 1;
       const task = await service.remove(taskId);
 
-      expect(task).toEqual(mockedTasks.find(({ id }) => id === taskId));
+      expect(task).toEqual(DEFAULT_TASKS.find(({ id }) => id === taskId));
     });
 
     it("should throw NotFoundException when task to remove does not exist", async () => {
